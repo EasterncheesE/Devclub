7f0c7c33100680ef31f2ed86371b2d613369859f
EasternCheesE <no@no.no>
2020-07-11 14:02:02 +0300

    changes to List

--- WC3/Java_homework/Basic/List/ListMain.java
+++ WC3/Java_homework/Basic/List/ListMain.java
@@ -11,36 +11,32 @@ public class ListMain {
         
         for ( int i = 0; i < size; i++ ) {
             aList.add(size-i);
-            bList.add(i);
+            bList.add(i+1);
         }
         System.out.println("Array size = " + aList.size());
         System.out.println("Array = " + aList);
         
         aList.remove(5);
         aList.trimToSize();
-        
+        System.out.println("Array = " + aList);
         aList.add(5,5);
-        
-        aList.indexOf(5);
+        System.out.println("Array = " + aList);
+        System.out.println("Index of 7 in list: " + aList.indexOf(7));
         
         aList.add(5);
-        
+        System.out.println("Array = " + aList);
         aList.remove(aList.size()-1);
-        
+        System.out.println("Array = " + aList);
         Collections.sort(aList);
+        System.out.println("Array = " + aList);
         
+        System.out.println("ArrayA = " + aList);
+        System.out.println("ArrayB = " + bList);
         if (aList.equals(bList)) {
             System.out.println("Yes, they are equal");
         } else {
             System.out.println("Not, they are not equal");
         }
-        
-        
-        // try {
-        //  aList.remove(1);
-        // } catch (IndexOutOfBoundsException e) {
-        //     System.out.println("Array doesn't have this index.");
-        // }
         
         Collections.sort(aList);
     }
