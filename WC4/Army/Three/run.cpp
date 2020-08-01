f6880e280dbaeb48da179ab6f49598c95acf8938
EasternCheesE <no@no.no>
2020-08-01 21:09:40 +0300

    [magic] added necro heal on kill to vampire template. Reworking project from scratch

--- WC4/Army/Three/run.cpp
+++ WC4/Army/Three/run.cpp
@@ -5,15 +5,19 @@
 
 int main() {
     Unit* bob = new Unit(std::string("Bob"), 500, 200);
+    Unit* patrick = new Unit(std::string("Patrick"), 500, 200);
     Vampire* frank = new Vampire(std::string("Frank"), 600, 250);
     
+    std::cout << patrick << std::endl;
     std::cout << bob << std::endl;
     std::cout << frank << std::endl;
     
-    for ( int i = 1; bob->getHP() > 0 && frank->getHP() > 0; i++ ) {
+    frank->attack(patrick);
+    
+    for ( int i = 1; bob->getHP() > 0 && patrick->getHP() > 0; i++ ) {
         std::cout << std::endl << "CYCLE NUMBER " << i << std::endl;
-        bob->attack(frank);
-        frank->attack(bob);
+        bob->attack(patrick);
+        patrick->attack(bob);
     }
     
     std::cout << std::endl << frank << std::endl;
