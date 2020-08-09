ebad2ef2ec27c6b247293a886a34398efb2191e7
EasternCheesE <no@no.no>
2020-08-09 13:45:51 +0300

    [magic] deleting all for fixing merge issues

--- WC4/Army/Three/Spell/Fireball.cpp
+++
@@ -1,9 +0,0 @@
-#include "Fireball.h"
-
-Fireball::Fireball(int manaCost, int spellValue) : DefaultSpell("Fireball", manaCost, spellValue) {}
-Fireball::~Fireball() {}
-
-void Fireball::useSpell(Spellcaster* owner, Unit* target) {
-    owner->reduceMP(this->manaCost);
-    target->takeMagicDMG(this->spellValue);
-}
