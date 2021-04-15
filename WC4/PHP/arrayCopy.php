b103d35c1b4e9c80394ac93cd7eacb018ea86b92
EasternCheese <no@no.no>
2021-04-15 13:57:09 +0300

    [FISHKARAS TEST]

--- WC4/PHP/arrayCopy.php
+++ WC4/PHP/arrayCopy.php
@@ -3,12 +3,11 @@
         $destination = $source;
     }
     
-    
-    $source = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,0];
+    $source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     $destination = array();
-    
-    
+    $source[3] = 99;
     arrayCopy($source, $destination);
+    
     
     print_r($destination);
 ?>
